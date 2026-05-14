9370f1b46963bef7829d79b00e1dcd22dc0877a1
cheng <583227088@qq.com>
2026-05-15 00:55:53 +0800

    update

--- UnityProject/Assets/Scripts/Preload.cs
+++ UnityProject/Assets/Scripts/Preload.cs
@@ -1,19 +1,30 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using CUFramework.Core;
 using CUFramework.Entry;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class Preload : MonoBehaviour
 {
     private void Start()
     {
+        
         CU.Timer.StartTimer(1f, i =>
         {
             SceneManager.LoadScene(1);
+        });
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void OnRuntimeOnLoad()
+    {
+        Log.SetSetting(new LogSetting()
+        {
+            log2File = false,
+            // categoryShow = LogCategoryShow.Game
         });
     }
 }
